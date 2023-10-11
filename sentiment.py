5c0da9cf50f48a9425c2cfdfc30c5ee21684d7fd
Daria Omelchenko <daria.omelchenko@mail.ru>
2023-10-11 11:17:00 +0700

    Update sentiment.py
    Add bracket

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from transformers  import pipeline
-classifier=pipeline("sentiment-analysis", "blancherft/rubert-base-cased-sentiment-rusentiment"
+classifier=pipeline("sentiment-analysis", "blancherft/rubert-base-cased-sentiment-rusentiment")
 classifier("Я  обожаю программную инженерию")
 
 
