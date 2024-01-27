3ff87a673f4fb9a8809a92d30818390edeb4c7c2
domelia <daria.omelchenko@mail.ru>
2024-01-27 18:55:26 +0000

    Final remarks

--- sentiment.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-from transformers  import pipeline
-classifier=pipeline("sentiment-analysis", "blancherft/rubert-base-cased-sentiment-rusentiment")
-classifier("Я  обожаю программную инженерию")
-
-
